5ad2240989a9de7395a4d71a577259e00ecbd234
Ritik Kadyan <kadyanepic@gmail.com>
2022-12-07 18:26:01 -0500

    Added more entities in models.py

--- main/models.py
+++ main/models.py
@@ -9,3 +9,33 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.f_name + ' ' + self.l_name
+
+
+class Game_studio(models.Model):
+    num_of_games_released = models.IntegerField()
+    studio_name = models.CharField(max_length=20)
+
+
+class Games(models.Model):
+    g_name = models.CharField(max_length=20)
+    g_qty_sold = models.IntegerField()
+    gs_id = models.ForeignKey(Game_studio, on_delete=models.CASCADE)
+
+
+# In-App Purchases
+class IAP(models.Model):
+    iap_name = models.CharField(max_length=20)
+    iap_cost = models.IntegerField()
+    g_id = models.ForeignKey(Games, on_delete=models.CASCADE)
+
+
+class Transaction(models.Model):
+    c_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    g_id = models.ForeignKey(Games, on_delete=models.CASCADE, blank=True)
+    iap_id = models.ForeignKey(IAP, on_delete=models.CASCADE, blank=True)
+
+
+class Employee(models.Model):
+    emp_first_name = models.CharField(max_length=20)
+    emp_last_name = models.CharField(max_length=20)
+    emp_pay = models.IntegerField()
